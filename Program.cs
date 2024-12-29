baf3cd24f487b5b7e36e43f9080ce207d5bb09ca
Adrian Seeley <contact@adrianseeley.com>
2024-12-29 14:18:47 -0500

    gini yields 66% on 1000/1000, going to try error count reduction

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿public class Program
+{
+    public static List<(List<float> input, int output)> ReadMNIST(string filename, int max = -1)
+    {
+        List<(List<float> input, int output)> data = new List<(List<float> input, int output)>();
+        string[] lines = File.ReadAllLines(filename);
+        for (int lineIndex = 1; lineIndex < lines.Length; lineIndex++) // skip headers
+        {
+            string line = lines[lineIndex].Trim();
+            if (line.Length == 0)
+            {
+                continue; // skip empty lines
+            }
+            string[] parts = line.Split(',');
+            int labelInt = int.Parse(parts[0]);
+            List<float> input = new List<float>();
+            for (int i = 1; i < parts.Length; i++)
+            {
+                input.Add(float.Parse(parts[i]));
+            }
+            data.Add((input, labelInt));
+            if (max != -1 && data.Count >= max)
+            {
+                break;
+            }
+        }
+        return data;
+    }
+
+    public static void Main(string[] args)
+    {
+        List<(List<float> input, int output)> mnistTrain = ReadMNIST("D:/data/mnist_train.csv", max: 1000);
+        List<(List<float> input, int output)> mnistTest = ReadMNIST("D:/data/mnist_test.csv", max: 1000);
+        Tree tree = Tree.Create(mnistTrain, minLeaf: 3);
+        int correct = 0;
+        int incorrect = 0;
+        foreach ((List<float> input, int output) in mnistTest)
+        {
+            int estimate = tree.Predict(input);
+            if (estimate == output)
+            {
+                correct++;
+            }
+            else
+            {
+                incorrect++;
+            }
+        }
+        float fitness = (float)correct / (float)(correct + incorrect);
+        Console.WriteLine($"Correct: {correct}, Incorrect: {incorrect}, Fitness: {fitness}");
+        Console.ReadLine();
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,200 @@
+﻿public class Tree
+{
+    public Tree? left;
+    public Tree? right;
+    public List<float>? leftCairn;
+    public List<float>? rightCairn;
+    public bool homogeneous;
+    public Dictionary<int, int> histogram;
+    public int estimate;
+    public float gini;
+
+    public static Tree Create(List<(List<float> input, int output)> samples, int minLeaf)
+    {
+        float[,] distances = new float[samples.Count, samples.Count];
+        for (int i = 0; i < samples.Count; i++)
+        {
+            Console.Write($"\rDistances {i + 1}/{samples.Count}");
+            for (int j = i; j < samples.Count; j++)
+            {
+                float distance = CalculateDistance(samples[i].input, samples[j].input);
+                distances[i, j] = distance;
+                distances[j, i] = distance;
+            }
+        }
+        Console.WriteLine();
+        List<int> indices = Enumerable.Range(0, samples.Count).ToList();
+        return new Tree(samples, indices, distances, minLeaf);
+    }
+
+    private Tree(List<(List<float> input, int output)> samples, List<int> indices, float[,] distances, int minLeaf)
+    {
+        // initialize null
+        this.left = null;
+        this.right = null;
+        this.leftCairn = null;
+        this.rightCairn = null;
+
+        histogram = CalculateHistogram(samples, indices);
+        estimate = CalculateEstimate(histogram);
+        gini = GINI(samples.Count, histogram);
+
+        // if the GINI is 0, we are done
+        if (gini == 0)
+        {
+            homogeneous = true;
+            return;
+        }
+        else
+        {
+            homogeneous = false;
+        }
+
+        // seach for best cairns
+        float bestSplitGINI = gini;
+        int bestLeftCairnIndex = -1;
+        int bestRightCairnIndex = -1;
+        List<int> leftIndices = new List<int>(indices.Count);
+        List<int> rightIndices = new List<int>(indices.Count);
+        for (int leftI = 0; leftI < indices.Count; leftI++)
+        {
+            Console.Write($"\rSplit {leftI + 1}/{indices.Count}");
+            int leftIndex = indices[leftI];
+            for (int rightI = leftI + 1; rightI < indices.Count; rightI++)
+            {
+                int rightIndex = indices[rightI];
+                leftIndices.Clear();
+                rightIndices.Clear();
+                foreach(int index in indices)
+                {                     
+                    if (distances[leftIndex, index] <= distances[rightIndex, index])
+                    {
+                        leftIndices.Add(index);
+                    }
+                    else
+                    {
+                        rightIndices.Add(index);
+                    }
+                }
+                if (leftIndices.Count < minLeaf || rightIndices.Count < minLeaf)
+                {
+                    continue;
+                }
+                Dictionary<int, int> leftHistogram = CalculateHistogram(samples, leftIndices);
+                Dictionary<int, int> rightHistogram = CalculateHistogram(samples, rightIndices);
+                float leftGINI = GINI(leftIndices.Count, leftHistogram);
+                float rightGINI = GINI(rightIndices.Count, rightHistogram);
+                float leftWeight = (float)leftIndices.Count / (float)indices.Count;
+                float rightWeight = (float)rightIndices.Count / (float)indices.Count;
+                float splitGINI = (leftWeight * leftGINI) + (rightWeight * rightGINI);
+                if (splitGINI < bestSplitGINI)
+                {
+                    bestSplitGINI = splitGINI;
+                    bestLeftCairnIndex = leftIndex;
+                    bestRightCairnIndex = rightIndex;
+                }
+            }
+        }
+        Console.WriteLine();
+
+        // if we found an improvement, recurse
+        if (bestSplitGINI < gini)
+        {
+            leftIndices.Clear();
+            rightIndices.Clear();
+            foreach(int index in indices)
+            {
+                if (distances[bestLeftCairnIndex, index] <= distances[bestRightCairnIndex, index])
+                {
+                    leftIndices.Add(index);
+                }
+                else
+                {
+                    rightIndices.Add(index);
+                }
+            }
+            leftCairn = new List<float>(samples[bestLeftCairnIndex].input);
+            rightCairn = new List<float>(samples[bestRightCairnIndex].input);
+            left = new Tree(samples, leftIndices, distances, minLeaf);
+            right = new Tree(samples, rightIndices, distances, minLeaf);
+        }
+    }
+
+    private static float CalculateDistance(List<float> a, List<float> b)
+    {
+        float distance = 0;
+        for (int i = 0; i < a.Count; i++)
+        {
+            float delta = a[i] - b[i];
+            distance += delta * delta;
+        }
+        return MathF.Sqrt(distance);
+    }
+
+    private Dictionary<int, int> CalculateHistogram(List<(List<float> input, int output)> samples, List<int> indices)
+    {
+        Dictionary<int, int> histogram = new Dictionary<int, int>();
+        foreach (int index in indices)
+        {
+            int output = samples[index].output;
+            if (!histogram.ContainsKey(output))
+            {
+                histogram[output] = 0;
+            }
+            histogram[output]++;
+        }
+        return histogram;
+    }
+
+    private int CalculateEstimate(Dictionary<int, int> histogram)
+    {
+        int bestOutput = -1;
+        int bestOutputCount = -1;
+        foreach (KeyValuePair<int, int> pair in histogram)
+        {
+            if (pair.Value > bestOutputCount)
+            {
+                bestOutput = pair.Key;
+                bestOutputCount = pair.Value;
+            }
+        }
+        return bestOutput;
+    }
+
+    private float GINI(int count, Dictionary<int, int> histogram)
+    {
+        float sumOfSquaredProbabilities = 0;
+        foreach (KeyValuePair<int, int> pair in histogram)
+        {
+            float probability = (float)pair.Value / (float)count;
+            sumOfSquaredProbabilities += probability * probability;
+        }
+        float gini = 1 - sumOfSquaredProbabilities;
+        return gini;
+    }
+
+    public int Predict(List<float> input)
+    {
+        if (left == null || right == null)
+        {
+            return estimate;
+        }
+        else
+        {
+            if (leftCairn == null || rightCairn == null)
+            {
+                throw new Exception("Cairns are null");
+            }
+            float distanceToLeftCairn = CalculateDistance(input, leftCairn);
+            float distanceToRightCairn = CalculateDistance(input, rightCairn);
+            if (distanceToLeftCairn <= distanceToRightCairn)
+            {
+                return left.Predict(input);
+            }
+            else
+            {
+                return right.Predict(input);
+            }
+        }
+    }
+}
